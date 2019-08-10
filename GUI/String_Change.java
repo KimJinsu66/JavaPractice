649160c4a6348ac1940cac0b424d38eaf1a47a91
KimJinsu66 <45025575+KimJinsu66@users.noreply.github.com>
2019-08-10 09:36:48 +0900

    String Integer Change code
    String -> Integer
    Integer -> String
    Change code

---
+++ GUI/String_Change.java
@@ -0,0 +1,15 @@
+package GUI;
+
+public class String_Change {
+
+	public static void main(String[] args) {
+		String from = "123";
+		int to = Integer.parseInt(from)	;
+		
+		int from_1 = 123;
+		String to_1 = Integer.toString(from_1);
+				
+	}
+
+}
+   
